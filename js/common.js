cc2a7e3eb8d9a30eb7b4284a3238d3f31aaa2588
irina ugrak <irina.ugrak@gmail.com>
2018-11-08 13:46:47 +0300

    8.11.2018

--- js/common.js
+++ js/common.js
@@ -2,17 +2,17 @@ $('.header-slider').slick({
     dots: true
 });
 
-// initMap() - функция инициализации карты
+// initMap() - function for initialization map
 function initMap() {
-    // Координаты центра на карте.
+    // Center coords at the map
     var coordinates = new google.maps.LatLng(59.985848, 30.399855);
-    // Обязательные опции с которыми будет проинициализированна карта
+    // Options for initialization
     var mapOptions = {
-        center: coordinates, // Координаты центра мы берем из переменной centerLatLng
-        zoom: 16,               // Зум по умолчанию. Возможные значения от 0 до 21
+        center: coordinates, // Center coordinates (var coordinates)
+        zoom: 16,               // Zoom Possible 0 to 21
         disableDefaultUI: true
     };
-    // Создаем карту внутри элемента #footerMap
+    // Create map into #footerMap element
     var map = new google.maps.Map(document.getElementById("footerMap"), mapOptions);
 
     image = 'img/decoration/marker.png',
@@ -22,17 +22,37 @@ function initMap() {
         icon: image
     });
 }
-// Ждем полной загрузки страницы, после этого запускаем initMap()
+// Wait page load, start initMap()
 google.maps.event.addDomListener(window, "load", initMap);
 
-/*
- image = 'images/marker.png',
-marker = new google.maps.Marker({
-    position: coordinates,
-    map: map,
-    icon: image
-});
- panControl: false,
-      zoomControl: false,
-      scaleControl: false,
-* */
+//show hide adaptive form
+let adaptiveForm = document.querySelector('#adaptiveForm');
+adaptiveForm.addEventListener('click', showFeedback);
+function showFeedback(){
+    //get page height
+    var pageHeight = Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+    );
+    //get visible page height (browser window)
+    var screenHeight = document.documentElement.clientHeight;
+    var hiddenFeedbackContainer = document.querySelector('#hiddenFeedbackContainer');
+    var hiddenFeedbackContainerForm = document.querySelector('#hiddenFeedbackContainerForm');
+    //show hidden element with form
+    hiddenFeedbackContainer.style.display = "block";
+    //set height and position for hidden elements
+    hiddenFeedbackContainerForm.style.top = pageHeight - screenHeight + "px";
+    hiddenFeedbackContainer.style.height = pageHeight + "px";
+    //add hide form function
+    var hiddenFeedbackContainerFormHide = document.querySelector('#hiddenFeedbackContainerFormHide');
+    document.body.addEventListener('click', hideOrderForm);
+    hiddenFeedbackContainerFormHide.addEventListener('click', hideOrderForm);
+    //hide form function
+    function hideOrderForm(e){
+        if(e.target.id == "hiddenFeedbackContainer" || e.target.id == "hiddenFeedbackContainerFormHide"){
+            hiddenFeedbackContainer.style.display = "none";
+        }
+    }
+};
+
